97535de05df91b787b7599adf6ade44e2ca92907
Mannat Dhawan <mannat301299@gmail.com>
2021-01-08 18:02:12 +0530

    jwt and cookie

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Response;
+use Symfony\Component\HttpFoundation\Cookie;
+
+use Laravel\Lumen\Routing\Controller as BaseController;
+
+class Controller extends BaseController
+{
+    protected function respondWithToken($token)
+    {
+        $res= new Response();
+    
+        $res->withCookie(new cookie('token',$token,45000));
+        echo $res;
+        return $res;
+    }
+
+    // [
+    //     'token' => $token,
+    //     'token_type' => 'bearer',
+    //     'expires_in' => Auth::factory()->getTTL() * 60
+    // ]
+}
